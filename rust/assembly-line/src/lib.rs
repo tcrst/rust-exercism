4d2cd6a6d45c0cc31e3a3afddac3e2c9e84a2995
tcrst <cristi.tarnita@gmail.com>
2021-09-06 10:50:22 +0000

    Add solution

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -5,18 +5,35 @@
 pub fn production_rate_per_hour(speed: u8) -> f64 {
     match speed {
         1..=4 => {
-              (speed * 221).into() 
-        }
-        5..=8 => { 
-              (speed * 221).into() 
-        }
-        9..=10 => { 
-              (speed * 221).into() 
-        }
-        _ => 0.0
+            let rate: f64 = 221.0;
+            speed as f64 * rate 
+        },
+        5..=8 => {
+            let rate: f64 = 198.9;
+            speed as f64 * rate 
+        },
+        9..=10 => {
+            let rate: f64 = 170.17;
+            speed as f64 * rate
+        },
+        _ => 0.0,
     }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    match speed {
+        1..=4 => {
+            let rate: u32 = 221;
+            speed as u32 * rate / 60 as u32
+        },
+        5..=8 => {
+            let rate: u32 = 198;
+            speed as u32 * rate / 60 as u32
+        },
+        9..=10 => {
+            let rate: u32 = 170;
+            speed as u32 * rate / 60 as u32
+        },
+        _ => 0,
+    }
 }
